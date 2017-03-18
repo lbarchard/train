d64620f1f214c27183ed22a4d572741738a8eb38
Barchard <lb185070@ncr.com>
2017-03-18 08:31:27 -0400

    making slow progress on time math

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,17 +38,26 @@ $( document ).ready(function() {
       var trainName = childSnapshot.val().trainName;
       var destination = childSnapshot.val().destination;
       var frequency = childSnapshot.val().frequency;
-      var firstTrainTime = moment(childSnapshot.val().firstTrainTime,"HH:mm"); //make sure it's a moment date
-      var hoursSinceFirstTrain = moment().subtract(firstTrainTime).hours(); //this doesn't work as it things firstTrainTime is midnight always
-      var minutesSinceFirstTrain = moment().subtract(firstTrainTime).minutes(); //this doesn't work as it things firstTrainTime is midnight always
+      
+      
+      //Convert the train time string into a moment object
+      var firstTrainTime = moment(childSnapshot.val().firstTrainTime,"HH:mm");
+
+      //Try to do math on the time now compared to the first train time - doesn't work :-(
+      var hoursSinceFirstTrain = moment().subtract(firstTrainTime).hours(); //this doesn't work as it thinks firstTrainTime is midnight always
+      var minutesSinceFirstTrain = moment().subtract(firstTrainTime).minutes(); //this doesn't work as it thinks firstTrainTime is midnight always
       var totalMinutesSinceFirstTrain = 60*(hoursSinceFirstTrain) + minutesSinceFirstTrain;
       // Created a variable to figure out when the next arrival is
-      var nextArrivalMinutes = frequency * (Math.floor(totalMinutesSinceFirstTrain / frequency) + 1) //this should get minutes from the original train that the next arrival is happening
+      var nextArrivalTotalMinutesFromFirstTrain = frequency * (Math.floor(totalMinutesSinceFirstTrain / frequency) + 1) //this should get minutes from the original train that the next arrival is happening
+      var nextArrivalHour = Math.floor(nextArrivalTotalMinutesFromFirstTrain/60);
+      var nextArrivalMinutes = nextArrivalTotalMinutesFromFirstTrain - nextArrivalHour*60
+      var nextArrivalTime = nextArrivalHour + ":" + nextArrivalMinutes
+      
       // Created a variable to figure out how many minutes away the next arrival is
-      var minutesAway = nextArrivalMinutes - totalMinutesSinceFirstTrain
+      var minutesAway = nextArrivalTotalMinutesFromFirstTrain - totalMinutesSinceFirstTrain
 
 
-      $("#schedule").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrivalMinutes + "</td><td>" + minutesAway + "</td><td>");
+      $("#schedule").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrivalTime + "</td><td>" + minutesAway + "</td><td>");
 
       }, function (errorObject) {
           console.log('The read failed' + errorObject.code);
